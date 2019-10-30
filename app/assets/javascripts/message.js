e846716afe2241c6a56b0bcafcaefcca8d2cdb36
hiiihaaa <snshiiihaaa@gmail.com>
2019-10-30 15:13:26 +0900

    DOM.val('') =>  DOM[0].reset()

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -38,7 +38,7 @@ $(function(){
       var html = buildHTML(data);
       $('.messages').append(html);
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
-      $('.form__message').val('');
+      $('.new_message')[0].reset();
       $('.form__submit').attr('disabled', false);
     })
     .fail(function(){
