8ff9488662f3a4721438af7a85d57892efc81a90
hiiihaaa <snshiiihaaa@gmail.com>
2019-11-05 01:48:24 +0900

    Revert "add-auto-deploy"
    This reverts commit 17c165aff19f9f6ce3380739f3a8a1f1146a9779.

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, "git@github.com:hiiihaaa/chat-space.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :rbenv_type, :user
-set :rbenv_ruby, '2.5.1'
+set :rbenv_ruby, '2.5.1p57'
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/key_pem.pem']
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
